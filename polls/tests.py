ecfe8f7467b97bb44e1eb43d395468db3880d806
Brett Goss <brettgoss@live.com>
2017-05-31 01:01:13 -0700

    Test past question results view

--- polls/tests.py
+++ polls/tests.py
@@ -133,3 +133,13 @@ class QuestionIndexResultsView(TestCase):
         url = reverse('polls:results', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_results_view_with_a_past_question(self):
+        """
+        The results view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text="Past question.", days=-5)
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
