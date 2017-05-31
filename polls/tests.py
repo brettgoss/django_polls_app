375f38f97100cb5ea75f1afba16fc069f721e09e
Brett Goss <brettgoss@live.com>
2017-05-30 18:23:42 -0700

    Create test for recent pub_date question

--- polls/tests.py
+++ polls/tests.py
@@ -26,3 +26,12 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(days=30)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
