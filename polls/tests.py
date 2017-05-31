4bf8c0ed19ca4558f9f4a0461c1191145e4f810b
Brett Goss <brettgoss@live.com>
2017-05-30 19:30:39 -0700

    Create Question view test if no questions

--- polls/tests.py
+++ polls/tests.py
@@ -4,7 +4,7 @@ import datetime
 
 from django.utils import timezone
 from django.test import TestCase
-
+from django.urls import reverse
 from .models import Question
 
 # Create your tests here.
@@ -35,3 +35,22 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
