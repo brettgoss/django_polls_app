7835086caec93edd410dd7979324d6805004cc6b
Brett Goss <brettgoss@live.com>
2017-05-30 23:47:16 -0700

    Create test for past question

--- polls/tests.py
+++ polls/tests.py
@@ -54,3 +54,17 @@ class QuestionViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the
+        index page.
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    
