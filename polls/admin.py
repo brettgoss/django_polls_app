42472e3da53c181e1d434b93807bf04b5638236b
Brett Goss <brettgoss@live.com>
2017-06-03 23:41:40 -0700

    Register choice model in admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
