8bdec233b1b4f43858414b19b50bc146cb1df478
Brett Goss <brettgoss@live.com>
2017-06-03 22:58:42 -0700

    Create admin class and change field order

--- polls/admin.py
+++ polls/admin.py
@@ -5,4 +5,7 @@ from django.contrib import admin
 from .models import Question
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
