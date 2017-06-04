8b6c006f090917b819b1245f9bb3c30b4a689c37
Brett Goss <brettgoss@live.com>
2017-06-03 23:39:32 -0700

    Create fieldset group

--- polls/admin.py
+++ polls/admin.py
@@ -6,6 +6,9 @@ from .models import Question
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
