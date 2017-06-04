808cae90cb47ba06364a4da77ea9269378caf168
Brett Goss <brettgoss@live.com>
2017-06-03 23:48:40 -0700

    Display choice admin tablular instead of stacked

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 # Register your models here.
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
